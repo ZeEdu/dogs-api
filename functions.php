b5d99cb3e77a27cb114f56eb821235cab775f54d
eduardo_Siqueira <eduardo.carlos.siqueira@gmail.com>
2021-03-30 15:40:20 -0300

    first commit

---
+++ functions.php
@@ -0,0 +1,48 @@
+<?php
+
+// remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+add_filter('rest_endpoints', function ($endpoints) {
+    unset($endpoints['/wp/v2/users']);
+    unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+    return $endpoints;
+});
+
+$dirbase = get_template_directory();
+
+require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+require_once $dirbase . '/endpoints/photo_delete.php';
+require_once $dirbase . '/endpoints/photo_get.php';
+
+require_once $dirbase . '/endpoints/comment_post.php';
+require_once $dirbase . '/endpoints/comment_get.php';
+
+require_once $dirbase . '/endpoints/stats_get.php';
+
+require_once $dirbase . '/endpoints/password.php';
+
+update_option('large_size_w', 1000);
+update_option('large_size_h', 1000);
+update_option('large_crop', 1);
+
+/**
+ * Troca a url da api
+ *
+ * @return string
+ * */
+function Change_api()
+{
+    return 'json';
+}
+
+add_filter('rest_url_prefix', 'Change_api');
+
+function expire_token()
+{
+    return time() + (60 * 60 * 24);
+}
+
+add_action('jwt_auth', 'expire_token');
